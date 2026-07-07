d012412657a0572855d52f5b8daa5dced5722284
Doug Zwick <modusponens@gmail.com>
2026-07-07 16:20:46 -0700

    Working on a sphere generation function

--- The Guardian Legend Remake/Assets/Editor/MeshAssetCreator.cs
+++ The Guardian Legend Remake/Assets/Editor/MeshAssetCreator.cs
@@ -9,7 +9,7 @@ public static class MeshAssetCreator
   {
     const string assetName = "Cone";
 
-    var divisions = 60;
+    var divisions = 24;
     
     var verts = new Vector3[divisions * 2 + 2];
     var tris = new int[3 * divisions * 2];
@@ -56,7 +56,7 @@ public static class MeshAssetCreator
   {
     const string assetName = "Pyramid";
 
-    var divisions = 60;
+    var divisions = 12;
 
     var sideVertCount = 3 * divisions;
     var baseVertCount = divisions + 1;
@@ -105,6 +105,98 @@ public static class MeshAssetCreator
       tris[baseTriIndex + 1] = baseStartIndex + prev;
       tris[baseTriIndex + 2] = baseStartIndex + i;
     }
+
+    CreateHelper(assetName, verts, tris);
+  }
+
+
+  // [MenuItem("Tools/Create Meshes/Create Teardrop")]
+  private static void CreateTeardrop()
+  {
+    const string assetName = "Pyramid";
+
+    var divisions = 12;
+    var slices = 3;
+
+    var tipVertCount = 1;
+    var hemisphereVertCount = divisions * slices;
+    var poleVertCount = 1;
+    var verts = new Vector3[tipVertCount + hemisphereVertCount + poleVertCount];
+    
+    var coneTriCount = divisions;
+    var hemisphereTriCount = divisions * slices * 2;
+    var tris = new int[coneTriCount + hemisphereTriCount];
+
+
+
+    CreateHelper(assetName, verts, tris);
+  }
+
+
+  [MenuItem("Tools/Create Meshes/Create Sphere")]
+  private static void CreateSphere()
+  {
+    var divisions = 12;
+    var slices = 3;
+    var assetName = $"Sphere{divisions}d{slices}s";
+
+    var ringCount = 2 * slices - 1;
+    var verts = new Vector3[ringCount * divisions + 2];
+    
+    var tris = new int[3 * divisions * slices * 2 * 2];
+
+    var r = 0.5f;
+    var deltaTheta = 2 * Mathf.PI / divisions;
+    var deltaRho = Mathf.PI / (2 * slices);
+    var topPoleIndex = 0;
+    var topRingStartIndex = topPoleIndex + 1;
+    var botRingStartIndex = divisions * (ringCount - 1) + 1;
+    var botPoleIndex = verts.Length - 1;
+
+    verts[topPoleIndex] = Vector3.up / 2;
+
+    for (var j = 0; j < ringCount; ++j)
+    {
+      var rho = deltaRho * j;
+      var y = r * (1 - Mathf.Cos(rho));
+
+      for (var i = 0; i < divisions; ++i)
+      {
+        var theta = deltaTheta * i;
+        var x = r * Mathf.Cos(theta);
+        var z = r * Mathf.Sin(theta);
+        var vert = new Vector3(x, y, z);
+        var vertIndex = j * divisions + i + 1;
+        verts[vertIndex] = vert;
+
+        var next = (i + 1) % divisions;
+
+        if (j == ringCount - 1) continue;
+
+        if (j == 0)
+        {
+          var topTriIndex = 3 * i;
+          tris[topTriIndex + 0] = topPoleIndex;
+          tris[topTriIndex + 1] = topRingStartIndex + next;
+          tris[topTriIndex + 2] = topRingStartIndex + i;
+
+          var botTriIndex = 3 * (ringCount * divisions + i);
+          tris[botTriIndex + 0] = botPoleIndex;
+          tris[botTriIndex + 1] = botRingStartIndex + next;
+          tris[botTriIndex + 2] = botRingStartIndex + i;
+        }
+
+        var quadIndex = 3 * divisions + 6 * (j * divisions + i);
+        tris[quadIndex + 0] = topRingStartIndex + j * divisions + i;
+        tris[quadIndex + 1] = topRingStartIndex + (j + 1) * divisions + next;
+        tris[quadIndex + 2] = topRingStartIndex + (j + 1) * divisions + i;
+        tris[quadIndex + 3] = topRingStartIndex + j * divisions + i;
+        tris[quadIndex + 4] = topRingStartIndex + j * divisions + next;
+        tris[quadIndex + 5] = topRingStartIndex + (j + 1) * divisions + next;
+      }
+    }
+
+    verts[botPoleIndex] = Vector3.down / 2;
 
     CreateHelper(assetName, verts, tris);
   }
