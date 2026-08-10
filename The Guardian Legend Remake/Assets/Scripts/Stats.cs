02b7fb9f9dd4d8bd53642d1f7517fbaa6657e4ee
Doug Zwick <modusponens@gmail.com>
2026-08-10 16:08:03 -0700

    Starting stats system

---
+++ The Guardian Legend Remake/Assets/Scripts/Stats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+public class Stats : MonoBehaviour
+{
+  [SerializeField]
+  private float m_Attack;
+  [SerializeField]
+  private float m_Defense;
+  [SerializeField]
+  private float m_MovementSpeed;
+  [SerializeField]
+  private float m_AttackRate;
+}
